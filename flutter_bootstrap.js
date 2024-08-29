b7674ac6feb4e51d2134276893d673834684339a
team-cheers <>
2024-08-29 09:26:52 +0000

    웹 배포시 Context Path 변경3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1325690982"
+    serviceWorkerVersion: "1957619507"
   }
 });
